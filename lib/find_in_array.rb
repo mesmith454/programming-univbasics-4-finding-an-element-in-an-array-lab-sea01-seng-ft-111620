27888bb6a23bc6341a9caa43e4b8dc10c5374f46
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-30 17:48:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,9 +3,8 @@ def find_element_index(array, value_to_find)
   while count < array.length do
     if array[count]  == value_to_find
       return count
-    else 
-      return nil
-    end
+
+    
     count += 1 
   end
   found_value_index
